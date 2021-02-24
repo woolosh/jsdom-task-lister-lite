6ca88a9dd0ba97f3a9b8fec6125baf77e373a780
woolosh <joshuapmorales@gmail.com>
2021-02-24 16:41:13 -0600

    basic requirements completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+
+  form.addEventListener("submit", addTask);
 });
+
+function addTask(e) {
+  e.preventDefault();
+  let newTask = e.target.newTaskDescription.value;
+  let listItem = document.createElement("li");
+  listItem.innerText = newTask;
+  console.log(listItem);
+  let task = document.getElementById("tasks");
+  task.append(listItem);
+}
